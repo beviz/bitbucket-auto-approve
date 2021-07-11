e13946bdfcb884fd2cd71830f4319e156af62f88
Zhijia Zhao <zhijia.zhao@thoughtworks.com>
2021-07-11 17:21:27 +0800

    use http proxy instead of socks since openconnect is already enabled http proxy.

--- common.js
+++ common.js
@@ -1,67 +1,62 @@
-const SocksProxyAgent = require('socks5-https-client/lib/Agent')
 const request = require('request')
 const config = require('./config')
 
 const getBitbuketRequestOptions = function() {
-  const defaultOptions = {
-    headers : {
-      'Content-Type': 'application/json'
-    },
-    agentClass: SocksProxyAgent,
-    agentOptions: {
-      socksHost: config.proxy.host,
-      socksPort: config.proxy.port
+    const defaultOptions = {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        proxy: `http://${config.proxy.host}:${config.proxy.port}`
     }
-  }
 
-  if (config.accessToken) {
-    defaultOptions.headers['Authorization'] = `Bearer ${config.accessToken}`
-  } else if (config.username) {
-    defaultOptions.auth = {
-      user: config.username,
-      password: config.password
+    if (config.accessToken) {
+        defaultOptions.headers['Authorization'] = `Bearer ${config.accessToken}`
+    } else if (config.username) {
+        defaultOptions.auth = {
+            user: config.username,
+            password: config.password
+        }
     }
-  }
 
-  return defaultOptions
+    return defaultOptions
 }
 
 const requestBitbucket = function(options, callback) {
-  options = { ...getBitbuketRequestOptions(), ...options }
-  request(options, callback)
+    options = {...getBitbuketRequestOptions(), ...options }
+    request(options, callback)
 }
 
 const requestServer = function(uri, options, callback) {
-  const defaultOptions = {
-    url: `${config.server}${uri}`,
-    headers: {
-      'Content-Type': 'application/json'
+    const defaultOptions = {
+        url: `${config.server}${uri}`,
+        headers: {
+            'Content-Type': 'application/json'
+        }
     }
-  }
-  options = { ...defaultOptions, ...options }
-  request(options, callback)
+    options = {...defaultOptions, ...options }
+    request(options, callback)
 }
 
 const convertToApiApproveUrl = function(url) {
-  const regex = '.+\.com/projects/(.+)/repos/(.+)/pull-requests/(.+)/overview'
-  const matches = url.match(regex)
-  if (!matches) {
-    return false
-  }
+    const regex = '.+\.com/projects/(.+)/repos/(.+)/pull-requests/(.+)/overview'
+    const matches = url.match(regex)
+    if (!matches) {
+        return false
+    }
 
-  const apiPartial = {
-    workspace: matches[1],
-    repo: matches[2],
-    prId: matches[3]
-  }
-  const apiUrl = new URL(url)
-  apiUrl.pathname = `/rest/api/latest/projects/${apiPartial.workspace}/repos/${apiPartial.repo}/pull-requests/${apiPartial.prId}/approve`
+    const apiPartial = {
+        workspace: matches[1],
+        repo: matches[2],
+        prId: matches[3]
+    }
+    const apiUrl = new URL(url)
+    apiUrl.pathname = `/rest/api/latest/projects/${apiPartial.workspace}/repos/${apiPartial.repo}/pull-requests/${apiPartial.prId}/approve`
 
-  return apiUrl.href
+    return apiUrl.href
 }
 
 module.exports = {
-  requestBitbucket,
-  requestServer,
-  convertToApiApproveUrl
+    requestBitbucket,
+    requestServer,
+    convertToApiApproveUrl
 }
